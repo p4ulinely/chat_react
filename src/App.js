bb33979e0a1a7c23b3d8d490cc5425d6e34c6998
p4ulinely <paulinelym@gmail.com>
2022-04-17 01:43:12 -0300

    chat-app simulando coleta de msgs com setInterval

--- src/App.js
+++ src/App.js
@@ -1,25 +1,107 @@
 import logo from './logo.svg';
 import './App.css';
+import { useEffect, useState, useContext, useRef } from 'react';
+import { ChatContext, ChatProvider } from './contexts/ChatContext';
+
+const estiloMain = {
+    textAlign: "center" 
+    
+}
+
+const Msg = ({body, user, ultimaMsg}) => {
+    const estiloMsg = {
+        borderStyle: 'solid',
+        marginTop: '1px'
+    }
+
+    return (
+        <div ref={ultimaMsg} style={estiloMsg}><b>{user.name}</b>: {body}</div>
+    );
+}
+
+const Chat = () => {
+    const lastMsgDiv = useRef(null)
+    const [bodyMessage, setBodyMessage] = useState('')
+    const {
+        msgs,
+        insereNovaMsg,        
+        coletaMsgs,
+        limpaFeedMsgs
+    } = useContext(ChatContext)
+
+    const handleSendMessageButton = (e) => {
+        const msg = bodyMessage.trim()
+
+        e.preventDefault();
+
+        if(msg === '')
+            return
+
+        insereNovaMsg(msg)
+        limpaMsg()
+    }
+    
+    const limpaMsg = () => setBodyMessage('')
+
+    const scrollToFeedEnd = () => {
+        lastMsgDiv.current?.scrollIntoView({ 
+            behavior: 'smooth',
+            block: 'start'
+        });
+    }
+
+    useEffect(() => {
+        coletaMsgs()
+        return limpaFeedMsgs       
+    }, [])
+    
+    useEffect(() => {
+        scrollToFeedEnd();
+    }, [msgs])
+
+    return (
+        <div>
+            <div className="Msgs-chat">
+                {msgs.length < 1 
+                    ? 'carregando msgs...'
+                    : msgs.map((msg, i) => (
+                        <Msg key={i} body={msg.body} user={msg.user} ultimaMsg={lastMsgDiv} />
+                    ))
+                }
+            </div>
+            <form>
+                <input value={bodyMessage} type="text" onChange={e => setBodyMessage(e.target.value)} placeholder='type ur msg...'/>
+                <button onClick={handleSendMessageButton}>enviar</button>
+            </form>
+        </div>
+    );    
+}
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [logged, setLogged] = useState(false)
+    
+    const handleJoinChatButton = () => {
+        setLogged(true);
+    }
+
+    const handleLeaveChatButton = () => {
+        setLogged(false);
+    }
+
+    return (
+        <div style={estiloMain}>
+            <img src={logo} className="App-logo" alt="App-logo" />
+            <h1>chat-app</h1>
+            {!logged && <button onClick={handleJoinChatButton}>join chat</button>}
+            {logged && 
+                <div>
+                    <button onClick={handleLeaveChatButton}>leave chat</button>
+                    <ChatProvider>
+                        <Chat />
+                    </ChatProvider>
+                </div>}
+        </div>
+    );
 }
 
 export default App;
---
+++ src/contexts/ChatContext.js
@@ -0,0 +1,46 @@
+import { useState, useRef, createContext } from "react";
+
+export const ChatContext = createContext();
+
+export const ChatProvider = ({children}) => {
+    const urlChat = process.env.REACT_APP_URL_CHAT
+    const [msgs, setMsgs] = useState([])
+    const temp = useRef(null)
+
+    const insereNovaMsg = (body) => {
+        const novaMsg = {body, user: { url: '', name: 'logged dude', color: '#000000'}}
+        setMsgs( prev => [...prev, novaMsg])
+    }
+    
+    const coletaMsgs = () => {
+        const msg1 = {body: 'oie caras..', user: { url: '', name: 'Dude1', color: '#000000'}}
+        const msg2 = {body: 'people soh bora..', user: { url: '', name: 'Dude2', color: '#555555'}}
+
+        temp.current = setInterval(() => {
+            setMsgs(prev => [...prev, msg1, msg2]) 
+            console.log('coletando msgs');
+        }, 2000)
+    }
+    
+    const limpaFeedMsgs = () => {
+        setMsgs([])
+
+        if (temp.current !== null)
+            clearInterval(temp.current)
+
+        console.log('limpou feed msgs');
+    }
+
+    return (
+        <ChatContext.Provider
+            value={{
+                msgs,
+                insereNovaMsg,
+                coletaMsgs,
+                limpaFeedMsgs
+            }}
+        >
+            {children}
+        </ChatContext.Provider>
+    );
+}
